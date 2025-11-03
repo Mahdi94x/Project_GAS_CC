21afc1a9a21187c9311619294f9280176b411059
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-11-03 19:49:45 +0200

    Enhanced Input and Player Controller
    - In the editor, created a mapping context for movement and 3 input action for look, move and jump
    - Created a C++ class based on UE APlayerController, have an array of input mapping context for movement and abilities, and private variable for the input action
    - The player controller class will handle the enhanced input creation and call back functions binding to avoid clutter in the CC_PlayerCharacter class
    - Created a blueprint BP_CC_PlayerController and updated the default game mode player controller

---
+++ Source/Project_GAS_CC/Private/Player/CC_PlayerController.cpp
@@ -0,0 +1,65 @@
+// Copyrights to Mahdi94x based on Course Make exciting multiplayer and single player games with the Gameplay Ability System in UE5 By Stephen Ulibarri
+
+#include "Player/CC_PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/Character.h"
+
+
+void ACC_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	
+	if (!IsValid(InputSubsystem)) return;
+	
+	for (UInputMappingContext* Context : InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context , 0);
+	}
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!IsValid(EnhancedInputComponent)) return;
+
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	
+}
+
+void ACC_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	GetCharacter()->Jump();
+}
+
+void ACC_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+	GetCharacter()->StopJumping();
+}
+
+void ACC_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	const FRotator YawRotation(0.f,GetControlRotation().Yaw,0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void ACC_PlayerController::Look(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
---
+++ Source/Project_GAS_CC/Public/Player/CC_PlayerController.h
@@ -0,0 +1,40 @@
+// Copyrights to Mahdi94x based on Course Make exciting multiplayer and single player games with the Gameplay Ability System in UE5 By Stephen Ulibarri
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CC_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class PROJECT_GAS_CC_API ACC_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+	TObjectPtr<UInputAction> LookAction;
+
+	void Jump();
+	void StopJumping();
+	void Move (const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	
+};
