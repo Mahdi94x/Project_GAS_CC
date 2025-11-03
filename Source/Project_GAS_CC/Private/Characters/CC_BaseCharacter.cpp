13ee50e6e385b47e9eea841921fedaf489118dbf
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-11-03 16:56:20 +0200

    Characters Creation
    - Added C++ class CC_BaseCharacter based on UE ACharacter class to be the parent for all characters in the project playable or enemies, adjusted bone transformation on a dedicated server in the construction and marked the class as Abstract
    - Added C++ class CC_PlayerCharacter based on CC_BaseCharacter , added a camera component and a camera boom and adjusted capsule and movement component settings in the constructor
    - Added BP_CC_PlayerCharacter based on CC_PlayerCharacter, adjusted the mesh and the animation blueprint
    - Added BP_CrashGameMode to be the default game mode for the project and updated the default pawn to BP_CC_PlayerCharacter
    - Fixed errors in the imported assets and made sure that the output log is free of any errors

---
+++ Source/Project_GAS_CC/Private/Characters/CC_BaseCharacter.cpp
@@ -0,0 +1,15 @@
+// Copyrights to Mahdi94x based on Course Make exciting multiplayer and single player games with the Gameplay Ability System in UE5 By Stephen Ulibarri
+
+#include "Project_GAS_CC/Public/Characters/CC_BaseCharacter.h"
+
+ACC_BaseCharacter::ACC_BaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	// Tick and refresh bone transform whether rendered or not - for bone updates on a dedicated server
+	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+}
+
+
+
+
---
+++ Source/Project_GAS_CC/Public/Characters/CC_BaseCharacter.h
@@ -0,0 +1,16 @@
+// Copyrights to Mahdi94x based on Course Make exciting multiplayer and single player games with the Gameplay Ability System in UE5 By Stephen Ulibarri
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CC_BaseCharacter.generated.h"
+
+UCLASS(Abstract)
+class PROJECT_GAS_CC_API ACC_BaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACC_BaseCharacter();
+};
